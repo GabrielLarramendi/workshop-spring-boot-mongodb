26147e9bd73a1dc801b77a7deaac06ebe8aeea60
GabrielLarramendi <larramendiga@gmail.com>
2023-03-03 09:57:08 -0300

    First commit

--- src/main/java/com/larramendi/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/larramendi/workshopmongo/WorkshopmongoApplication.java
@@ -3,6 +3,7 @@ package com.larramendi.workshopmongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class WorkshopmongoApplication {
 
